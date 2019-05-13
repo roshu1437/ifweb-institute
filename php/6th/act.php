7954514646737ede45f66718f6f67452a9084322
Roshu1437 <roshukiemail@gmail.com>
2019-05-13 08:51:31 +0500

    add lecture php 6

---
+++ php/6th/act.php
@@ -0,0 +1,40 @@
+<?php 
+	// for($i=0; $i <= 10; $i++) { 
+	// 	echo $i.'<br>';
+	// }
+	$i=1;
+	$c=1;
+	// while ($i<=10){
+	// 	echo $c.'<br>';
+	// 	$c++;
+	// }
+	$array = array('name'=>'my name is ali','age'=>'20','edu'=>'FA');
+	$student = array('ali','asad','akram','talha','khuram','faizan');
+	// echo $array['name'];
+	// $a=1;
+	// foreach($array as $k => $v) {
+	// 	echo 'index ('.$k.') value ('.$v.')<br>';
+	// 	// echo 'index ('.$a.') value ('.$v.')<br>';
+	// 	// $a++;
+	// }
+	// foreach($student as $k => $v) {
+	// 	echo 'index ('.$k.') value ('.$v.')<br>';
+	// }
+
+	$a='my name is ali';
+	$b='my name is asad';
+	$c='my name is akram';
+	// echo $a.'<br>';
+	// echo $b;
+	// echo $c;
+function e(){
+	echo 'test';
+}
+function eb($v){
+	echo $v.'<br>';
+}
+	eb($a);
+	eb($b);
+	eb($c);
+
+?>
