c59c813aa0983438323155a1b9b6ad76af7bba47
Roshu1437 <roshukiemail@gmail.com>
2019-05-28 08:52:28 +0500

    grid layout

--- php/13th/index.php
+++ php/13th/index.php
@@ -6,11 +6,169 @@
 </head>
 <body>
 	<?php require_once('inc/header.php'); ?>
-	<div class="center-align">
-		<a class="btn waves-effect btn-small fs30">Home Button</a>
-		<a class="btn waves-effect">Home Button</a>
-		<a class="btn waves-effect btn-large">Home Button</a>
-		<a class="btn waves-effect waves-light">Home Button</a>
+	
+	<div class="container">
+		<table class="striped highlight centered responsive-table">
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+			<tr>
+				<td>Table Text 1</td>
+				<td>Table Text 2</td>
+				<td>Table Text 3</td>
+				<td>Table Text 4</td>
+				<td>Table Text 5</td>
+			</tr>
+		</table>
+		<div class="row">
+			<div class="col s12 m6 l4 light-green">
+				<h1>My name is ali</h1>
+			</div>
+			<div class="col s12 m6 l4 green darken-4">
+				<h1>My name is ali</h1>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col s12 m8 l4 green accent-4">
+				<h1>My name is ali</h1>
+			</div>
+			<div class="col s12 m8 l4 green accent-4">
+				<h1>My name is ali</h1>
+			</div>
+		</div>
+		<h1 class="right-align red">My name is ali</h1>
+		<h1 class="right green">My name is ali</h1>
+		<div class="clearfix"></div>
+		<h1 class="">My name is ali</h1>
 	</div>
 	<?php require_once('inc/footer.php'); ?>
 </body>
