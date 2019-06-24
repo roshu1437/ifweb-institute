4d897c4d43cc49bfdcfef08c29dc5047b0a3656a
Roshu1437 <roshukiemail@gmail.com>
2019-06-24 21:42:36 +0500

    user signup with same email validation

---
+++ php/17th/action.php
@@ -0,0 +1,46 @@
+<?php 
+	$db=mysqli_connect('localhost','root','','webeg');
+	if(!$db){
+		echo 'Server is busy';
+		die;
+	}
+	if(isset($_POST['signup'])){
+		if(!empty($_POST['f_name']) and !empty($_POST['l_name']) and !empty($_POST['email']) and !empty($_POST['pass']) and !empty($_POST['repass'])){
+			$fname=$_POST['f_name'];
+			$lname=$_POST['l_name'];
+			$email=$_POST['email'];
+			$pass=$_POST['pass'];
+			$rpass=$_POST['repass'];
+			if($pass == $rpass){
+				// where query
+				//$q='SELECT email FROM user WHERE email="'.$email.'"';
+				$q="SELECT email FROM user WHERE email=$email";
+				$qr=mysqli_query($db,$q);
+				if($qr){
+					if(mysqli_num_rows($qr) < 1){
+						$ins='INSERT INTO user SET
+							fname="'.$fname.'",
+							lname="'.$lname.'",
+							email="'.$email.'",
+							password="'.$pass.'"
+						';
+						$ins_run=mysqli_query($db,$ins);
+						if($ins_run){
+							echo 'User registerd';
+						}else{
+							echo "please try again";
+						}
+					}else{
+						echo 'This email already taken';
+					}
+				}else{
+					echo 'Server is much busy';
+				}
+			}else{
+				echo 'Please Enter Same Password.';
+			}
+		}else{
+			echo "Please Fill All the Fields.";
+		}
+	}
+?>
---
+++ php/17th/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form SignUp</title>
+</head>
+<body>
+	<form action="action.php" method="post">
+		<input type="text" name="f_name" placeholder="First Name"><br><br>
+		<input type="text" name="l_name" placeholder="Last Name"><br><br>
+		<input type="email" name="email" placeholder="Enter Email"><br><br>
+		<input type="password" name="pass" placeholder="Enter Password"><br><br>
+		<input type="password" name="repass" placeholder="Enter Password Again"><br><br>
+		<input type="submit" name="signup" value="SignUp">
+	</form>
+</body>
+</html>
