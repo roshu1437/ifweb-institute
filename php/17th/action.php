5fba74c5f2b234df5361088d6301d30a8631bff0
Roshu1437 <roshukiemail@gmail.com>
2019-06-25 21:31:37 +0500

    login

--- php/17th/action.php
+++ php/17th/action.php
@@ -4,6 +4,7 @@
 		echo 'Server is busy';
 		die;
 	}
+	// For user signup 
 	if(isset($_POST['signup'])){
 		if(!empty($_POST['f_name']) and !empty($_POST['l_name']) and !empty($_POST['email']) and !empty($_POST['pass']) and !empty($_POST['repass'])){
 			$fname=$_POST['f_name'];
@@ -13,8 +14,7 @@
 			$rpass=$_POST['repass'];
 			if($pass == $rpass){
 				// where query
-				//$q='SELECT email FROM user WHERE email="'.$email.'"';
-				$q="SELECT email FROM user WHERE email=$email";
+				$q='SELECT email FROM user WHERE email="'.$email.'"';
 				$qr=mysqli_query($db,$q);
 				if($qr){
 					if(mysqli_num_rows($qr) < 1){
@@ -22,7 +22,7 @@
 							fname="'.$fname.'",
 							lname="'.$lname.'",
 							email="'.$email.'",
-							password="'.$pass.'"
+							password="'.md5($pass).'"
 						';
 						$ins_run=mysqli_query($db,$ins);
 						if($ins_run){
@@ -43,4 +43,30 @@
 			echo "Please Fill All the Fields.";
 		}
 	}
+
+	// for user login
+	if(isset($_POST['login'])){
+		if(!empty($_POST['login_email']) && !empty($_POST['login_pass'])){
+			$email=$_POST['login_email'];
+			$pass=md5($_POST['login_pass']);
+
+			$q='SELECT * FROM user WHERE email="'.$email.'" AND password="'.$pass.'"';
+			$qr=mysqli_query($db,$q);
+			if($qr){
+				if(mysqli_num_rows($qr) > 0){
+					echo 'user login';
+				}else{
+					echo "Email and password not match";
+				}
+			}else{
+				echo "Please try again";
+			}
+		}else{
+			echo 'Please Fill All the fields';
+		}
+	}
+
+
+
+
 ?>
--- php/17th/index.php
+++ php/17th/index.php
@@ -2,15 +2,32 @@
 <html>
 <head>
 	<title>Form SignUp</title>
+<style>
+.signup,.login{
+	width: 50%;
+	float: left;
+}
+</style>
 </head>
 <body>
-	<form action="action.php" method="post">
-		<input type="text" name="f_name" placeholder="First Name"><br><br>
-		<input type="text" name="l_name" placeholder="Last Name"><br><br>
-		<input type="email" name="email" placeholder="Enter Email"><br><br>
-		<input type="password" name="pass" placeholder="Enter Password"><br><br>
-		<input type="password" name="repass" placeholder="Enter Password Again"><br><br>
-		<input type="submit" name="signup" value="SignUp">
-	</form>
+	<div class="signup">
+		<h1>Signup</h1>
+		<form action="action.php" method="post">
+			<input type="text" name="f_name" placeholder="First Name"><br><br>
+			<input type="text" name="l_name" placeholder="Last Name"><br><br>
+			<input type="email" name="email" placeholder="Enter Email"><br><br>
+			<input type="password" name="pass" placeholder="Enter Password"><br><br>
+			<input type="password" name="repass" placeholder="Enter Password Again"><br><br>
+			<input type="submit" name="signup" value="SignUp">
+		</form>
+	</div>
+	<div class="login">
+		<h1>Login</h1>
+		<form action="action.php" method="post">
+			<input type="email" name="login_email" placeholder="Enter Email"><br><br>
+			<input type="password" name="login_pass" placeholder="Enter Password"><br><br>
+			<input type="submit" name="login" value="Login">
+		</form>
+	</div>
 </body>
 </html>
