d201b690aab7ba9963a42f18ec1204b331575d67
Roshu1437 <roshukiemail@gmail.com>
2019-07-02 21:20:56 +0500

    sql left join

--- php/18th/action.php
+++ php/18th/action.php
@@ -1,13 +1,25 @@
 <?php 
+	$i=1;
+	while (1==1) {
+		echo $i.'-> '.time().'<br>';
+		$i++;
+	}
+	die;
 	$db=mysqli_connect('localhost','root','','webeg');
 	if(!$db){
 		echo 'Server is busy';
 		die;
 	}
 	// $q='SELECT * FROM user LIMIT 3,2';
-	// $q='SELECT * FROM user order by id desc';
+	$q='SELECT * FROM user LEFT JOIN cat ON user.id=cat.user_id where user.id="3"';
+
+	$q='SELECT * FROM user where id="3"';
 	$qr=mysqli_query($db,$q);
-	while ($res=mysqli_fetch_assoc($qr)) {
-		echo "<pre>";print_r($res);echo "</pre>";
+	$res=mysqli_fetch_assoc($qr);
+
+	$q1='SELECT * FROM cat where user_id="'.$res['id'].'"';
+	$qr1=mysqli_query($db,$q1);
+	while ($res1=mysqli_fetch_assoc($qr1)) {
+		echo "<pre>";print_r($res1);echo "</pre>";
 	}
 ?>
