0eb81dd79d101dda6d6c5e58c9578f17002fa69a
Roshu1437 <roshukiemail@gmail.com>
2019-08-09 21:05:12 +0500

    Add chat system

--- project/application/views/inc/footer.php
+++ project/application/views/inc/footer.php
@@ -39,6 +39,15 @@ $(document).ready(function(){
         }
     });
 });
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5d271cd722d70e36c2a54000/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
 </script>
 
 
